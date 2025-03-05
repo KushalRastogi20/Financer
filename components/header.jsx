7c9140c52d8ea6062661d0c2adfdb0014668e793
KushalRastogi20 <138278696+KushalRastogi20@users.noreply.github.com>
2025-03-05 15:47:21 +0530

    User autherntication using clerk

---
+++ components/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { SignedIn, SignedOut,UserButton,SignInButton,SignUpButton } from '@clerk/nextjs'
+const Header = () => {
+  return (
+    <div>
+      <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+    </div>
+  )
+}
+
+export default Header
